730fd6686c8fd50f978573e0a349fcfadc749b01
An Nguyen <an_nguyen@datahouse.com>
2025-04-14 16:30:42 +0700

    feat(cart): try update image alt

--- libs/cart/cart-page/src/lib/cart-cart-page/cart-cart-page.tsx
+++ libs/cart/cart-page/src/lib/cart-cart-page/cart-cart-page.tsx
@@ -96,7 +96,7 @@ export const CartCartPage = () => {
         <StyledLi key={item.productId}>
           <a href={`/product/${item.productId}`}>
             <figure>
-              <img src={getProduct(productsState, item.productId).image} />
+              <img src={getProduct(productsState, item.productId).image} alt=''/>
             </figure>
           </a>
           <a href={`/product/${item.productId}`} className="title">
